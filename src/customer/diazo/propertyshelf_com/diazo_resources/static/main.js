23352fd56ac7f192e77937c3cf48f928e77e20b6
Kantanart <kantanat@propertyshelf.com>
2015-10-21 18:44:22 +0700

    Improve listing search with java , clean rules
    - clean rules
    - style about listing search (on carousel)
    - add javascript for improve listing search

--- src/customer/diazo/propertyshelf_com/diazo_resources/static/main.js
+++ src/customer/diazo/propertyshelf_com/diazo_resources/static/main.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
-    if($('.searchbox').length > 0){
-		$('.searchbox').insertAfter('.carousel-banners');
-    }
+	search_tile = $('.on-carousel .listing-search-tile');
+		if(search_tile.length>0){
+			p_min=search_tile.find('#formfield-form-widgets-price_min label');
+			p_max=search_tile.find('#formfield-form-widgets-price_max label');
+			p_min.hide();
+			p_max.hide();
+			// now the bedrooms
+			b_label=search_tile.find('#formfield-form-widgets-beds label');
+			b_text_min =b_label.text().trim()+' (Min)';
+			b_text_max =b_label.text().trim()+' (Max)';
+			b_label.hide();
+			search_tile.find('#form-widgets-beds-min option[value=--MINVALUE--]').text(b_text_min);
+			search_tile.find('#form-widgets-beds-max option[value=--MAXVALUE--]').text(b_text_max);
+	}
 });
